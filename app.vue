6d6b2dcd25f3ba7286c3c973967b4cfe32330af6
Maxence Raynal <maxence.raynal@epitech.digital>
2023-12-05 16:41:46 +0100

    feat:base style

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    class: 'dark:bg-black',
+  },
+  bodyAttrs: {
+    class: 'bg-gray-50 dark:bg-black',
+  },
+});
+</script>
