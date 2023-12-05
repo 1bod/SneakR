a65482eb8a18b08bccda087e3a11c406a10ad10b
Maxence Raynal <maxence.raynal@epitech.digital>
2023-12-05 11:32:35 +0100

    feat: plugins install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    modules: [
+        "@nuxt/ui",
+        "@nuxt/image",
+        "@nuxtjs/supabase",
+        "@nuxtjs/tailwindcss",
+        "@nuxtjs/google-fonts",
+    ],
+    supabase: {
+        redirect: false,
+    },
+    googleFonts: {
+        download: true,
+        families: {
+            "DM Sans": [400, 600, 700],
+        },
+        preload: true,
+    },
+});
